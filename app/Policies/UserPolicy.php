988b1892587a67290c842abf9f70c0a701426a68
Юра Гуцалюк <hutsaluck@gmail.com>
2023-08-21 08:54:37 +0300

    Restrict add/edit/delete buttons

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,24 +2,23 @@
 
 namespace App\Policies;
 
-use App\Models\Order;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class OrderPolicy
+class UserPolicy
 {
     /**
      * Determine whether the user can view any models.
      */
     public function viewAny(User $user): bool
     {
-        return $user->is_admin == 1;
+        //
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Order $order): bool
+    public function view(User $user, User $model): bool
     {
         //
     }
@@ -29,38 +28,38 @@ class OrderPolicy
      */
     public function create(User $user): bool
     {
-        return $user->is_admin == 1;
+        //
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Order $order): bool
+    public function update(User $user, User $model): bool
     {
-        return $user->is_admin == 1;
+        return $user->is_admin;
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Order $order): bool
+    public function delete(User $user, User $model): bool
     {
-        return $user->is_admin == 1;
+        //
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Order $order): bool
+    public function restore(User $user, User $model): bool
     {
-        return $user->is_admin == 1;
+        //
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Order $order): bool
+    public function forceDelete(User $user, User $model): bool
     {
-        return $user->is_admin == 1;
+        //
     }
 }
